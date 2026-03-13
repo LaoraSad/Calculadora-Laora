807b7dde6b2e3fd11ed0ad1df11e0a5301b21783
LaoraSad <264025963+LaoraSad@users.noreply.github.com>
2026-03-13 10:58:33 -0500

    cambios

--- main.py
+++ main.py
@@ -10,26 +10,32 @@
 
 #figuras_2D_3D=["circulo", "cuadrado", "trapecio","rombo","triangulo rectangulo","cubo", "esfera", "pirámide","cono"]
 #figuras_3D=["cubo", "esfera", "pirámide","cono"]
+formulas_2D=["area"]
+formulas_3D=[]
+continuar="si"
+while continuar == "si":
+    print("----CALCULADORA GEOMETRICA\n")
+    print("----------opciones disponibles de figuras 2D------------\n")
+    print(
+        "circulo\n"
+        "cuadrado\n"
+        "trapecio\n"
+        "rombo\n"
+        "triangulo\n" 
+        "rectangulo\n"
+        )
+    print("-----------opciones disponibles de figuras 3D------------\n")
+    print(
+        "cubo\n"
+        "esfera\n"
+        "prirámide\n"
+        "cono\n"
+        )
+    
+    figura=int((input("Qué tipo de figuras deseas?\n 1(2D) o 2(3D)")))
+    if figura= circilos
+        print("las formulas disponibles")
 
-print("----CALCULADORA GEOMETRICA\n")
-print("----------opciones disponibles de figuras 2D------------\n")
-print(
-    "circulo\n"
-    "cuadrado\n"
-    "trapecio\n"
-    "rombo\n"
-    "triangulo\n" 
-    "rectangulo\n"
-    )
-print("-----------opciones disponibles de figuras 3D------------\n")
-print(
-    "cubo\n"
-    "esfera\n"
-    "prirámide\n"
-    "cono\n"
-    )
-
-figura=int((input("Qué tipo de figuras deseas?\n 1(2D) o 2(3D)")))
 
 
 
