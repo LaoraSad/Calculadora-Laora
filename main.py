388ea9dec55655716fb9e51ca41c5c1e62415925
LaoraSad <264025963+LaoraSad@users.noreply.github.com>
2026-03-13 10:46:55 -0500

    cambios

---
+++ main.py
@@ -0,0 +1,35 @@
+#calcular formulas de 4 figuras 2D (area, perometro) y 4 figuras 3D(volumen) y un triangulo rectangulo(angulo)
+#saber qué figuras voy a utilizar y qué formulas quiero que saque por cada figura
+#necesito funciones para cada formula de las figuras(puedes usar los datos enseguida)
+#uso while para mostras el menu y pedirle al usuario que figura desea conocer, el usuario debe colocar los valores (poner el tipo de numero) y yo los voy a poner en la formula que me pida
+#cuando escoja la figura necesito que se muestre que formulas estan disponibles
+#cuando escoja la formula y muestre el resultado preguntar si quiere escoger otra figura
+#TENER EN CUENTA: el usuario debe poner los parametros para rellenar la formula, debe saber que va a colocar un numero
+#usar try para que no me rompa nada o usar else
+
+
+#figuras_2D_3D=["circulo", "cuadrado", "trapecio","rombo","triangulo rectangulo","cubo", "esfera", "pirámide","cono"]
+#figuras_3D=["cubo", "esfera", "pirámide","cono"]
+
+print("----CALCULADORA GEOMETRICA\n")
+print("----------opciones disponibles de figuras 2D------------\n")
+print(
+    "circulo\n"
+    "cuadrado\n"
+    "trapecio\n"
+    "rombo\n"
+    "triangulo\n" 
+    "rectangulo\n"
+    )
+print("-----------opciones disponibles de figuras 3D------------\n")
+print(
+    "cubo\n"
+    "esfera\n"
+    "prirámide\n"
+    "cono\n"
+    )
+
+figura=int((input("Qué tipo de figuras deseas?\n 1(2D) o 2(3D)")))
+
+
+
